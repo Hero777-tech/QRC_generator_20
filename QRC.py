3bcbc17371ade4986a3bb34a324cf29190247c8d
Aditya Nath <68598911+Hero777-tech@users.noreply.github.com>
2022-05-16 19:45:08 +0300

    updated

--- QRC.py
+++ QRC.py
@@ -1,11 +1,11 @@
 import pyqrcode 
 from pyqrcode import QRCode 
   
-# String which represent the QR code 
-s = "https://www.youtube.com/"
+# This String will represent the QR code 
+s = "https://www.youtube.com/" #or any link
   
-# Generate QR code 
+# QR code will be generated 
 url = pyqrcode.create(s) 
   
-# Create and save the png file naming "myqr.png" 
-url.svg("myyoutube.svg", scale = 8)
+# Create and save the png file naming "myqr.png or my.svg...etc..." 
+url.svg("yt.svg", scale = 8)
